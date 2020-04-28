3f1a1cbc83cec3ff7d231d470d5a6fcb59a35499
Yili He <eli.he999@hotmail.com>
2020-04-28 16:13:26 +0800

    Update index.js

--- src/page/Phonebook/index.js
+++ src/page/Phonebook/index.js
@@ -16,44 +16,49 @@ import './index.css'
 
 var builder = require('xmlbuilder');
 
-const Phonebook = () => {
+class Phonebook extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 };
+    this.data = [
+      'Racing car sprays burning fuel into crowd.',
+      'Japanese princess to wed commoner.',
+      'Australian walks 100km after outback crash.',
+      'Man charged over missing wedding girl.',
+      'Los Angeles battles huge wildfires.',
+    ];
+  };
 
-  const xml = builder.create('root')
-    .ele('xmlbuilder')
-    .ele('repo', {'type': 'git'}, 'git://github.com/oozcitak/xmlbuilder-js.git')
-    .end({ pretty: true });
 
-  const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
-  
-  return (
-    <div>
-      <Header />
-      <Row>
-        <Col span={10} >
-          <nav class="itemlist">
-            <ul>
-              <li><a >Home</a></li>
-              <li><a >About</a></li>
-              <li><a >Work</a></li>
-              <li><a >Contact</a></li>
-            </ul>
-          </nav>
-        </Col>
-        <Col span={6} >
-          <p>{xml}</p>
-        </Col>
-        <Col span={8} >
-          <p>{xml}</p>
-        </Col>
-      </Row>
-    </div>
-  );
+  // const xml = builder.create('root')
+  //   .ele('xmlbuilder')
+  //   .ele('repo', {'type': 'git'}, 'git://github.com/oozcitak/xmlbuilder-js.git')
+  //   .end({ pretty: true });
+  render() {
+    return (
+      <div>
+        <Header />
+        <Row>
+          <Col span={10} >
+            <nav class="itemlist">
+              <ul>
+                <li><a >Home</a></li>
+                <li><a >About</a></li>
+                <li><a >Work</a></li>
+                <li><a >Contact</a></li>
+              </ul>
+            </nav>
+          </Col>
+          <Col span={6} >
+          
+          </Col>
+          <Col span={8} >
+          
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 };
 
 export default Phonebook;
